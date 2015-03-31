3c0464b83c82f8d67c684b4afce4424c36ea4feb
yura <reznikov_yura@mail.ru>
2015-03-31 23:05:45 +0300

    second commit

--- lib/src/led.cpp
+++ lib/src/led.cpp
@@ -152,13 +152,13 @@ LedState Led::Off()
 	return state;
 }
 
-LedState Led::Toggle()
-{
-	if(state == OFF || state == ON || state == INIT)
-	{
-		GPIO_WriteBit(port, pin, (BitAction) !GPIO_ReadOutputDataBit(port, pin));
-		state = (LedState) (GPIO_ReadOutputDataBit(port, pin) ^ offLevel);
-	}
-	return state;
-}
+//LedState Led::Toggle()
+//{
+//	if(state == OFF || state == ON || state == INIT)
+//	{
+//		GPIO_WriteBit(port, pin, (BitAction) !GPIO_ReadOutputDataBit(port, pin));
+//		state = (LedState) (GPIO_ReadOutputDataBit(port, pin) ^ offLevel);
+//	}
+//	return state;
+//}
 
